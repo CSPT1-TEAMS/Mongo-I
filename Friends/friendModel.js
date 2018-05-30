ffee5e5ad05d680e4d1688a9544d4a087f737404
unknown <alexisreyes90@gmail.com>
2018-05-29 23:28:28 -0400

    FriendModel implemented

--- Friends/friendModel.js
+++ Friends/friendModel.js
@@ -1,8 +1,28 @@
-// {
-//     firstName: "Jane", // String, required
-//     lastName: "Doe",  // String, required
-//     age: 18, // Number, required, should be an integer between 1 and 120
-//     createdOn: Mon Aug 14 2017 12:50:16 GMT-0700 (PDT) // Date, required, defaults to current date
-//   }
-
 const mongoose = require('mongoose');
+
+// schema design
+
+const friendDefinition = {
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    age: {
+        type: String,
+        required: true,
+    },
+    createdOn: {
+        type: Date,
+        default: Date.now,
+    },
+};
+
+const friendSchema = new mongoose.Schema(friendDefinition);
+
+const friendModel = mongoose.model('Friend', friendDefinition, 'friends');
+
+module.exports = friendModel;
