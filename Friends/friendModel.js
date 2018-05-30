37eb97b03c5a9fe5f0ccdfe7de011464e6646ca5
unknown <alexisreyes90@gmail.com>
2018-05-29 23:17:31 -0400

    Server.js boilerplate complete

---
+++ Friends/friendModel.js
@@ -0,0 +1,8 @@
+// {
+//     firstName: "Jane", // String, required
+//     lastName: "Doe",  // String, required
+//     age: 18, // Number, required, should be an integer between 1 and 120
+//     createdOn: Mon Aug 14 2017 12:50:16 GMT-0700 (PDT) // Date, required, defaults to current date
+//   }
+
+const mongoose = require('mongoose');
