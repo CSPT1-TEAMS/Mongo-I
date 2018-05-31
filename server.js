1b50e511f7070a03fe1b0c80ebb0ee481558f50b
mixelpixel <mixelpix@mac.com>
2018-05-31 01:31:33 -0400

    doodle bops

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const server = express();
+
+
+const friendsController = require('./friendsController');
+const friendModel = require('./friendModel');
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+
+mongoose
+  .connect('mongodb://localhost/friendsdb')
+  .then(mongo => {
+    console.log('Connected to DB!')
+  })
+
+server.use('/api/friends', friendsController);
 
 server.get('/', (req, res) => {
   res.status(200).json({ api: 'running' });
