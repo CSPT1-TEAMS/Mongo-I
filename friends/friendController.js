50e6e9175debc9d3d7f239bee9c178b4c6473587
Charlie <ccariste@gmail.com>
2018-05-30 20:55:16 -1000

    CRC: get and post functionality working, but error w post when age is required

---
+++ friends/friendController.js
@@ -0,0 +1,40 @@
+const router = require('express').Router();
+const Friend = require('../friends.js');
+
+router
+  .route('/')
+  .get((req, res) => {
+    Friend.find()
+      .then( friends => {
+        res.status(200).json(friends);
+      })
+      .catch( err => {
+        res.status(500).json({ error: 'Error getting freinds', err});
+      })
+    })
+
+  .post((req, res) => {
+    const { body } = res;
+    const friend = new Friend(body);
+    friend.save()
+      .then( friend => {
+        res.status(201).json(friend);
+      })
+      .catch( err => {
+        res.status(500).json({error: 'Error posting to database', err});
+      })
+  });
+
+router
+  .route('/:id')
+  .get((req, res) => {
+    res.status(200).json({ route: '/api/friends/' + req.params.id });
+  })
+  .delete((req, res) => {
+    res.status(200).json({ status: 'please implement DELETE functionality' });
+  })
+  .put((req, res) => {
+    res.status(200).json({ status: 'please implement PUT functionality' });
+  });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,17 +3,20 @@ const helmet = require('helmet');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/beardb').then(mongo => {
+mongoose.connect('mongodb://localhost/frienddb').then(mongo => {
   console.log('Connected to DB', mongo)
 }).catch(err => {
   console.log('Error connecting to DB', err)
 });
+
+const friendController = require('./friends/friendController.js');
 
 const server = express();
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+server.use('/api/friends', friendController);
 
 server.get('/', (req, res) => {
   res.status(200).json({ api: 'running' });
