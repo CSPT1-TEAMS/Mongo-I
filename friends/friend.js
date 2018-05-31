3aeb9468dbe8ea2bacbe49141dcee17bb9c49891
Lisa Canini <lisaceedesign@gmail.com>
2018-05-30 20:34:44 -0700

    CRUD working

---
+++ friends/friend.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const definition = {
+    firstName: {
+        type: String,
+        required: [true, "First name is required"]
+    },
+    lastName: {
+        type: String,
+        required: [true, "Last name is required"]
+    },
+    age: {
+        type: Number,
+        required: true,
+        min: [1, "Age must be greater than 1"],
+        max: [120, "Age must be less than 1"]
+    },
+    createdOn: {
+        type: Date,
+        default: Date.now,
+    }
+}
+
+const options = {
+    timestamps: true
+}
+
+const friendSchema = new mongoose.Schema(definition, options);
+
+module.exports = mongoose.model('Friend', friendSchema);
---
+++ friends/friendController.js
@@ -0,0 +1,81 @@
+const router = require('express').Router();
+
+const Friend = require('./friend');
+
+router.route('/')
+    .get((req, res) => {
+        Friend
+            .find()
+            .then(friends => {
+                res.status(200).json(friends);
+            })
+            .catch(err => {
+                res.status(500).json({ errorMessage: "The friends information could not be retrieved." })
+            })
+    })
+    .post((req, res) => {
+        const friendData = req.body;
+        const friend = new Friend(friendData);
+
+        friend
+            .save()
+            .then(friend => {
+                res.status(201).json(friend)
+            })
+            .catch(err => {
+                res.status(500).json(err);
+            });
+    });
+
+router.route('/:id')
+    .get((req, res) => {
+        const friendId = req.params.id;
+        
+        Friend.findById(friendId)
+            .then(friend => {
+                if (friend !== null) {
+                    res.status(200).json(friend);
+                } else {
+                    res.status(404).json({ message: "The friend with the specified ID does not exist." });
+                }
+            })
+            .catch(err => {
+                res.status(500).json({ errorMessage: "The friend information could not be retrieved." })
+            })
+    }) 
+    .delete((req, res) => {
+        const friendId = req.params.id;
+
+        Friend.findByIdAndRemove(friendId)
+            .then(deleted => {
+                if (deleted !== null) {
+                    res.status(200).json(deleted)
+                } else {
+                    return res.status(404).json({ message: "The friend with that ID does not exist" })
+                }
+            })
+            .catch(err => {
+                res.status(500).json({ error: "The friend could not be removed" })
+            })
+    })  
+    .put((req, res) => {
+        const friendId = req.params.id;
+        const friendData = req.body;
+        const options = {
+            new: true,
+        }
+
+        Friend.findByIdAndUpdate(friendId, friendData, options)
+            .then(friend => {
+                if (friend !== null) {
+                    res.status(200).json(friend);
+                } else {
+                    res.sendStatus(404).json({ message: "The friend with the specified ID does not exist." });
+                }
+            })
+            .catch(err => {
+                res.status(500).json(err);
+            })
+    }) ;
+
+    module.exports = router;
