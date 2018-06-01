6d8758a98466d4c2482727346da9cd603baf83d4
JohnCoronel <john.m.coronel@gmail.com>
2018-05-31 20:33:15 -0700

    Fix error handling

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/friendsDB')
+const db = mongoose.connection;
+db.on('error',console.error.bind(console,'MongoDB Connection Error:'));
+const friends = require('./friendsController');
 
 const server = express();
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+server.use('/api/friends',friends)
 
 server.get('/', (req, res) => {
   res.status(200).json({ api: 'running' });
